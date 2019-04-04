8c0873bb069c5133a048112c1c29596c804c2517
xiaofei <lixiaofei0627@gmail.com>
2019-04-04 17:30:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app" class="box"></div>
+</template>
+
+<script>
+// variables in  js
+import variables from "./variables";
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      variables: { ...variables }
+    };
+  },
+  created() {
+    console.log(this.variables, "App-this.variables");
+  }
+};
+</script>
+
+<!--css -->
+<style lang="css">
+#app {
+  height: 500px;
+}
+@import "./styles/css-use.css";
+</style>
+
+
+<!--scss -->
+<style lang="scss">
+@import "./styles/scss-use.scss";
+</style>
+
+<!-- less-->
+<style lang="less">
+@import "./styles/less-use.less";
+</style>
---
+++ src/variables/index.js
@@ -0,0 +1,10 @@
+import variablesScss from "./scss.scss";
+import variablesLess from "./less.less";
+import variablesCss from "./css.css";
+
+// 导出变量
+export default {
+  variablesScss,
+  variablesLess,
+  variablesCss
+};
